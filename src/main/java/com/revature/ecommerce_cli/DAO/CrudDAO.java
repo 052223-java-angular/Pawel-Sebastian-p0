4c819f71ccc5835ec093122b7657a2339c02decd
Sebastian Shader <sebfumaster@aol.com>
2023-05-24 18:33:56 -0700

    add User and Product DAO skeletons

--- src/main/java/com/revature/ecommerce_cli/DAO/CrudDAO.java
+++ src/main/java/com/revature/ecommerce_cli/DAO/CrudDAO.java
@@ -1,15 +1,16 @@
 package com.revature.ecommerce_cli.DAO;
+import java.util.Optional;
 
 import java.util.List;
 
 public interface CrudDAO<T> {
     void save(T obj);
 
-    void update(String id);
+    void update(T obj);
 
     void delete(String id);
 
-    T findById(String id);
+    Optional<T> findById(String id);
 
     List<T> findAll();
 }
