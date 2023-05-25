89571bb7b6b202c91d1c9dc971af873da4fc2a79
Seb Shader <sebfumaster@aol.com>
2023-05-25 00:31:37 -0700

    better DBTest

--- src/test/java/com/revature/ecommerce_cli/DBTest.java
+++ src/test/java/com/revature/ecommerce_cli/DBTest.java
@@ -9,19 +9,19 @@ import org.junit.Test;
 
 public class DBTest {
     @Test
-    public void connectionTest() {
+    public void connectionTest() throws IOException, SQLException, ClassNotFoundException {
         try (Connection cnctTest = ConnectionFactory.getInstance().getConnection()) {
             System.out.println("postgres connection made");
             assertNotNull(cnctTest);
         } catch (IOException e) {
             System.out.println("couldn't open db.properties");
-            System.out.println(e.getMessage());
+            throw e;
         } catch (SQLException e) {
             System.out.println("couldn't open connection");
-            System.out.println(e.getMessage());
+            throw e;
         } catch (ClassNotFoundException e) {
             System.out.println("couldn't find postgres driver");
-            System.out.println(e.getMessage());
+            throw e;
         }
     }
 }
