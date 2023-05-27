c4cadbb723c1f405608b8f676dbbf7420c6af1ff
Sebastian Shader <sebfumaster@aol.com>
2023-05-26 22:24:36 -0700

    just don't store users at all

--- src/main/resources/db/dml.sql
+++ src/main/resources/db/dml.sql
@@ -1,10 +1,10 @@
 set search_path to ecommerce_cli;
 
-insert into users (id, username, password)
-values --password: timmmmmm
-	('1540a4f8-2659-4375-be78-73910ed86d78', 'bobbbbbb', '$2a$10$lidqtfcaQifiDu2oGEkz4ONaVczA1Ye8lDKQ3qBHcJiKhcajhdAVa'),
-	--password: yahoo123
-	('346d35c4-d0b9-4976-92c0-7f7368656c87', 'yahoo123', '$2a$10$M.nGTjv.6K3m7tOLaKdc5.U6jrHIz/BVdPS0UkGkTLR5aS55z5qIq');
+-- register user with id: user1 first (edit db manually)
+-- 1st run ddl
+-- then register a new user
+-- change the new user's id to 'user1'
+-- run this script
 
 INSERT INTO products (id,"name",category,price,in_stock) VALUES
 	 ('jill','parasol','accessories',2,70000),
@@ -17,13 +17,13 @@ INSERT INTO products (id,"name",category,price,in_stock) VALUES
 
 --bobbbbbb's cart
 INSERT INTO cart_products (id,user_id,product_id,quantity) VALUES
-	 ('larry','1540a4f8-2659-4375-be78-73910ed86d78','june',2),
-	 ('gertrude','1540a4f8-2659-4375-be78-73910ed86d78','jill',40000),
-	 ('harry','1540a4f8-2659-4375-be78-73910ed86d78','will',1);
+	 ('larry','user1','june',2),
+	 ('gertrude','user1','jill',40000),
+	 ('harry','user1','will',1);
 
 INSERT INTO orders (id,user_id,amount,time_placed) VALUES
-	 ('zimm','1540a4f8-2659-4375-be78-73910ed86d78',7500,'1975-08-20 04:12:48.000'),
-	 ('pam','1540a4f8-2659-4375-be78-73910ed86d78',200000,'2020-01-08 01:45:22.000');
+	 ('zimm','user1',7500,'1975-08-20 04:12:48.000'),
+	 ('pam','user1',200000,'2020-01-08 01:45:22.000');
 
 INSERT INTO order_products (id,order_id,product_id,quantity) VALUES
 	 ('barry','zimm','will',993),
@@ -32,6 +32,6 @@ INSERT INTO order_products (id,order_id,product_id,quantity) VALUES
 	 ('doug','pam','sandy',777000);
 
 INSERT INTO reviews (id,rating,"comment",user_id,product_id) VALUES
-	 ('fran',1,'gets everywhere','1540a4f8-2659-4375-be78-73910ed86d78','sandy'),
-	 ('jordan',4,'verrry vorpal','1540a4f8-2659-4375-be78-73910ed86d78','will'),
-	 ('ted',3,'I can''t see anything!','1540a4f8-2659-4375-be78-73910ed86d78','billy');
+	 ('fran',1,'gets everywhere','user1','sandy'),
+	 ('jordan',4,'verrry vorpal','user1','will'),
+	 ('ted',3,'I can''t see anything!','user1','billy');
