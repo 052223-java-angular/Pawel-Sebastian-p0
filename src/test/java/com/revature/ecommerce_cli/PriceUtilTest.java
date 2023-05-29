79c38d84d3c92e8d88a4e0ca36fba5f2274a9fa1
Sebastian Shader <sebfumaster@aol.com>
2023-05-28 19:36:06 -0700

    PriceUtil round to 0 test

--- src/test/java/com/revature/ecommerce_cli/PriceUtilTest.java
+++ src/test/java/com/revature/ecommerce_cli/PriceUtilTest.java
@@ -15,6 +15,7 @@ public class PriceUtilTest {
         assertEquals(100500, PriceUtil.toCents("1005"));
         assertEquals(100400, PriceUtil.toCents("1004."));
         assertEquals(4, PriceUtil.toCents(".04"));
+        assertEquals(0, PriceUtil.toCents("0.00008"));
         assertEquals(14, PriceUtil.toCents(".14"));
         assertEquals(14, PriceUtil.toCents(".1409"));
         assertEquals(10, PriceUtil.toCents(".1009"));
