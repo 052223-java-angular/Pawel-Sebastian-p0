8915cd2bc0e2e95c335f7caaff1d6ef812ef5a2f
Sebastian Shader <sebfumaster@aol.com>
2023-05-29 15:54:06 -0700

    fix errant 'log' in ProductScreen

--- src/main/java/com/revature/ecommerce_cli/screens/ProductScreen.java
+++ src/main/java/com/revature/ecommerce_cli/screens/ProductScreen.java
@@ -21,7 +21,6 @@ public class ProductScreen implements IScreen{
     public void start(Scanner scan) {
     
     String input = "";
-    log
     while(true){
         logger.debug("Redrawing");
         clearScreen();
